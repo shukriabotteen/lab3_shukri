6fdc3fbb6009150f29680afba643a0ad2473ae1e
Abotteen <sabotte@ilstu.edu>
2026-02-01 13:36:39 -0600

    test2

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 echo "Welcome to lab 3 form <Place your name here>";
 echo "Welcome to lab 3 form <shukri>";
+echo "Welcome to lab 3 form <pc>";
 https://github.com/shukriabotteen/lab3_shukri
 ?>
